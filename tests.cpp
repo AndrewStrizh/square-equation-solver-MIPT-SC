a85ed5d943112c1da688bc4eae6c64e05a1dcc75
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-28 23:03:31 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,139 +1,81 @@
-/**
- *\file
- * File test.cpp a file storing functions for testing
- */
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include "tests.h"
 
-
-#ifndef HEADER_H_INCLUDED
-#define HEADER_H_INCLUDED
-
-
-
-
-
-#define ERROR_COLOR(str)  \
-    RED_TEXT str RESET_COLOR
-
-#define CORRECT_COLOR(str)  \
-    GREEN_TEXT str RESET_COLOR
-
-#define SMALL_ERROR_COLOR(str)  \
-    YELLOW_TEXT str RESET_COLOR
-
-
-
-/// Macro that creates the test_solve_square structure and starts testing
-#define TEST_SOLVE_SQUARE(a,b,c,x1,x2,nRoots) ({                    \
-    struct test_solve_square t = {{a,b,c},{x1,x2},nRoots};              \
-    run_test_solve_square(t, &correct_answers, &all_answers, filename);})
-
-
-/// Macro that creates variables for the number of all tests and valid tests
-#define BEGIN_TEST             \
-    int correct_answers = 0;   \
-    int all_answers = 0;
-
-
-/// Macro that outputs variable numbers of all tests and valid tests to the console
-#define END_TEST                                                                                   \
-    printf(GREEN_TEXT "passed %d tests out of %d" RESET_COLOR, correct_answers, all_answers);
-
-
-#define RED_TEXT "\x1b[31m"
-#define GREEN_TEXT "\x1b[32m"
-#define YELLOW_TEXT "\x1b[33m"
-#define RESET_COLOR "\x1b[0m"
-
-
-const double EPS_TEST = 1e-3;/**< Accuracy of numeric data type in test comparisons */
-
-
-/**
- *\enum
- * Possible number of roots
- */
-enum ROOT_KEYS
+int equate_double_test(const double x, const double y)
 {
-    ZERO_ROOTS,
-    ONE_ROOT,
-    TWO_ROOTS,
-    INF_ROOTS
-};
+    assert(isfinite(x));
+    assert(isfinite(y));
+
+    return fabs(x - y) < EPS_TEST;
+}
 
 
-/**
- *Function for solving a quadratic equation
- *\param p - structure stores the parameters of the quadratic equation
- *\param *r - structure storing the values of the roots of a quadratic equation
- */
-ROOT_KEYS solve_square (const struct Params p, struct Roots *r);
-
-
-/**
- *Function for solving a quadratic equation
- *\param p - structure stores the parameters of the quadratic equation
- *\param *r - structure storing the values of the roots of a quadratic equation
- */
-ROOT_KEYS solve_linear(const double b, const double c, double *x1);
-
-
-/**
- *Function for comparing two answers (the correct answer and the answer of the function) of type double
- *\param x,y are the numbers being compared
- */
-int equate_double_test(const double x, const double y);
-
-
-/**
- *Function that checks whether the test results match the results of the solve_square function
- *\param t - structure storing the test parameters of the quadratic equation, the correct roots and their number
- *\param *filename - the address of the file, name entered by the user
- *\param *all_answers - number of all tests
- */
-int test_solve(const struct test_solve_square t,const char* filename, int* all_answers);
-
-
-/**
- *Function that outputs information about the test to the console
- *\param t - structure storing the test parameters of the quadratic equation, the correct roots and their number
- *\param *correct_answers - number of valid tests
- *\param *all_answers - number of all tests
- */
-int run_test_solve_square (const struct test_solve_square t, int *correct_answers, int *all_answers);
-
-/**
- *Function that starts the execution of tests
- *\param *filename - the address of the file, name entered by the user
- */
-int all_tests(const char *filename);
-
-/**
- *\brief This structure stores the parameters of the quadratic equation
- */
-struct Params
+int run_test_solve_square(
+    const struct test_solve_square t, int* correct_answers, int* all_answers, const char* filename)
 {
-    double a, b, c;
-};
+    assert(correct_answers);
+    assert(all_answers);
+    assert(filename);
+    assert(isfinite(t.p.a));
+    assert(isfinite(t.p.b));
+    assert(isfinite(t.p.c));
+    assert(isfinite(t.r.x1));
+    assert(isfinite(t.r.x2));
 
+    if (test_solve(t, filename, all_answers) == 0)
+    {
+        printf(CORRECT_COLOR("test passed\n"));
+        ++(*correct_answers);
+    }
+    ++(*all_answers);
+    return 0;
+}
 
-
-/**
- *\brief This structure storing the values of the roots of a quadratic equation
- */
-struct Roots
+int test_solve(const struct test_solve_square t, const char* filename, int* all_answers)
 {
-    double x1, x2;
-};
+    assert(filename);
+    assert(all_answers);
+    assert(isfinite(t.p.a));
+    assert(isfinite(t.p.b));
+    assert(isfinite(t.p.c));
+    assert(isfinite(t.r.x1));
+    assert(isfinite(t.r.x2));
 
-/**
- *\brief Structure storing the test parameters of the quadratic equation, the correct roots and their number
- */
-struct test_solve_square
+    struct Roots x = {};
+    int nRoots = solve_square(t.p, &x);
+    if (t.nRootsRef != nRoots             || 
+        !equate_double_test(x.x1, t.r.x1) || 
+        !equate_double_test(x.x2, t.r.x2))
+    {
+        printf(ERROR_COLOR("FAILURE TEST %d: a=%lf; b = %lf; c = %lf\n"), *all_answers, t.p.a,
+            t.p.b, t.p.c);
+        FILE* tmp = fopen(filename, "a");
+        fprintf(tmp,
+            "FAILURE TEST №%d: a=%lf; b = %lf; c = %lf\nright x1 = %lf\tright x2 = %lf\tright"
+            "number of roots = %d\nprog x1 = %lf\tprog x2 = %lf\tprog number of roots = %d\n\n",
+            *all_answers, t.p.a, t.p.b, t.p.c, t.r.x1, t.r.x2, t.nRootsRef, x.x1, x.x2, nRoots);
+        fclose(tmp);
+        return 1;
+    }
+    else
+        return 0;
+}
+
+int all_tests(const char* filename)
 {
-    struct Params p;
-    struct Roots r;
-    int nRootsRef;
-};
-
-
-#endif // HEADER_H_INCLUDED
+    BEGIN_TEST;
+    FILE* tmp = fopen(filename, "w");
+    TEST_SOLVE_SQUARE(10000, 1, 0, 0, 0, 1);
+    TEST_SOLVE_SQUARE(1, 0, 0, 0, 0, 1);
+    TEST_SOLVE_SQUARE(0, 0, 1, 0, 0, 0);
+    TEST_SOLVE_SQUARE(-1, 0, 0, 0, 0, 1);
+    TEST_SOLVE_SQUARE(0, -1, 0, 0, 0, 1);
+    TEST_SOLVE_SQUARE(1, 2, -3, -3, 1, 2);
+    TEST_SOLVE_SQUARE(1, 2, -4, -3.2361, 1.2361, 2);
+    TEST_SOLVE_SQUARE(1, 0, -16, -4, 4, 1);
+    fclose(tmp);
+    END_TEST;
+    return 0;
+}
