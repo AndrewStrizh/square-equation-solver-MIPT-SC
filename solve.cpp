eace7c4d1e6e82847a6485bdf02469eee34bf6dc
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-28 09:23:54 +0300

    Update solve.cpp

--- solve.cpp
+++ solve.cpp
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <stdio.h>
 #include "solve.h"
+
+
 int equality_double(const double x, const double y)
 {
     assert(isfinite (x));
@@ -18,11 +20,14 @@ ROOT_KEYS solve_square (const struct st_params p, struct st_roots *r)
     assert(isfinite (p.a));
     assert(isfinite (p.b));
     assert(isfinite (p.c));
-    assert(isfinite (r -> x1));
-    assert(isfinite (r -> x2));
     assert(!isnan (p.a));
     assert(!isnan (p.b));
     assert(!isnan (p.c));
+    assert(r);
+    assert(&(r -> x1));
+    assert(&(r -> x2));
+    assert(isfinite (r -> x1));
+    assert(isfinite (r -> x2));
     assert(!isnan (r -> x1));
     assert(!isnan (r -> x2));
 
@@ -31,7 +36,7 @@ ROOT_KEYS solve_square (const struct st_params p, struct st_roots *r)
     else
     {
         double a2 = p.a * 2;
-        const double discr = p.b * p.b - 2 * a2 * p.c;
+        const double discr = p.b * p.b - 4 * p.a * p.c;
         if(equality_double(discr, 0))
         {
             if (equality_double(p.b, 0))
@@ -55,12 +60,14 @@ ROOT_KEYS solve_square (const struct st_params p, struct st_roots *r)
 
 ROOT_KEYS solve_linear(const struct st_params p, struct st_roots *r)
 {
+    assert(r);
+    assert(&(r -> x1));
+    assert(isfinite (r -> x1));
+    assert(!isnan (r -> x1));
     assert(isfinite (p.b));
     assert(isfinite (p.c));
-    assert(isfinite (r->x1));
     assert(!isnan (p.b));
     assert(!isnan (p.c));
-    assert(!isnan (r->x1));
 
     if(equality_double(p.b, 0))
     {
@@ -72,40 +79,42 @@ ROOT_KEYS solve_linear(const struct st_params p, struct st_roots *r)
     else
     {
         if (equality_double(p.c, 0))
-            r->x1 = p.c;
+            r -> x1 = p.c;
         else
-            r->x1 = -p.c / p.b;
+            r -> x1 = -p.c / p.b;
         return ONE_ROOT;
     }
 }
 
 int solver(const struct st_params params, struct st_roots *roots)
 {
+    assert(roots);
+    assert(&(roots -> x1));
+    assert(&(roots -> x2));
     assert(isfinite (params.a));
     assert(isfinite (params.b));
     assert(isfinite (params.c));
-    assert(isfinite (roots->x1));
-    assert(isfinite (roots->x2));
+    assert(isfinite (roots -> x1));
+    assert(isfinite (roots -> x2));
     assert(!isnan (params.a));
     assert(!isnan (params.b));
     assert(!isnan (params.c));
-    assert(!isnan (roots->x1));
-    assert(!isnan (roots->x2));
-
+    assert(!isnan (roots -> x1));
+    assert(!isnan (roots -> x2));
 
     int nRoots = solve_square (params, roots);
     switch (nRoots)
         {
-        case ZERO_ROOTS: printf("There are no roots\n");
+        case ZERO_ROOTS: printf(TEXT_COLOR(YELLOW_TEXT, "There are no roots\n"));
                 break;
 
-        case ONE_ROOT: printf("x = %lf\n", roots->x1);
+        case ONE_ROOT: printf(TEXT_COLOR(GREEN_TEXT, "x = %lf\n"), roots->x1);
                 break;
 
-        case TWO_ROOTS: printf("x1 = %lf\tx2 = %lf\n", roots->x1, roots->x2);
+        case TWO_ROOTS: printf(TEXT_COLOR(GREEN_TEXT, "x1 = %lf\tx2 = %lf\n"), roots -> x1, roots -> x2);
                 break;
 
-        case INF_ROOTS: printf("Number of roots: infinity");
+        case INF_ROOTS: printf(TEXT_COLOR(YELLOW_TEXT, "Number of roots: infinity\n"));
                 break;
 
         default:
