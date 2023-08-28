c2da1dae87f12024da810566648dbedb0d1ae899
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-28 20:19:47 +0300

    Update solve.cpp

--- solve.cpp
+++ solve.cpp
@@ -4,52 +4,41 @@
 #include "solve.h"
 
 
-int equality_double(const double x, const double y)
+int equate_zero(const double x)
 {
     assert(isfinite (x));
-    assert(isfinite (y));
-    assert(!isnan (x));
-    assert(!isnan (y));
-
-    return fabs(x - y) < EPS_ALL;
+    return fabs(x - 0) < EPS_ALL;
 }
 
 
-ROOT_KEYS solve_square (const struct st_params p, struct st_roots *r)
+ROOT_KEYS solve_square (const struct Params p, struct Roots *r)
 {
     assert(isfinite (p.a));
     assert(isfinite (p.b));
     assert(isfinite (p.c));
-    assert(!isnan (p.a));
-    assert(!isnan (p.b));
-    assert(!isnan (p.c));
     assert(r);
-    assert(&(r -> x1));
-    assert(&(r -> x2));
     assert(isfinite (r -> x1));
     assert(isfinite (r -> x2));
-    assert(!isnan (r -> x1));
-    assert(!isnan (r -> x2));
 
-    if(equality_double(p.a, 0))
+    if (equate_zero(p.a))
         return solve_linear(p, r);
     else
     {
-        double a2 = p.a * 2;
+        double aa = p.a * 2;
         const double discr = p.b * p.b - 4 * p.a * p.c;
-        if(equality_double(discr, 0))
+        if (equate_zero(discr))
         {
-            if (equality_double(p.b, 0))
+            if (equate_zero(p.b))
                 r -> x1 = p.b;
             else
-                r -> x1 = -p.b / a2;
+                r -> x1 = -p.b / aa;
             return ONE_ROOT;
         }
-        else if(discr > EPS_ALL)
+        else if (discr > EPS_ALL)
         {
             const double sq_discr = sqrt(discr);
-            r -> x1=(-p.b - sq_discr) / a2;
-            r -> x2=(-p.b + sq_discr) / a2;
+            r -> x1=(-p.b - sq_discr) / aa;
+            r -> x2=(-p.b + sq_discr) / aa;
             return TWO_ROOTS;
         }
         else
@@ -58,27 +47,23 @@ ROOT_KEYS solve_square (const struct st_params p, struct st_roots *r)
 }
 
 
-ROOT_KEYS solve_linear(const struct st_params p, struct st_roots *r)
+ROOT_KEYS solve_linear(const struct Params p, struct Roots *r)
 {
     assert(r);
-    assert(&(r -> x1));
     assert(isfinite (r -> x1));
-    assert(!isnan (r -> x1));
     assert(isfinite (p.b));
     assert(isfinite (p.c));
-    assert(!isnan (p.b));
-    assert(!isnan (p.c));
 
-    if(equality_double(p.b, 0))
+    if (equate_zero(p.b))
     {
-        if(equality_double(p.c, 0))
+        if (equate_zero(p.c))
             return INF_ROOTS;
         else
             return ZERO_ROOTS;
     }
     else
     {
-        if (equality_double(p.c, 0))
+        if (equate_zero(p.c))
             r -> x1 = p.c;
         else
             r -> x1 = -p.c / p.b;
@@ -86,35 +71,28 @@ ROOT_KEYS solve_linear(const struct st_params p, struct st_roots *r)
     }
 }
 
-int solver(const struct st_params params, struct st_roots *roots)
+int solve(const struct Params params, struct Roots *roots)
 {
     assert(roots);
-    assert(&(roots -> x1));
-    assert(&(roots -> x2));
     assert(isfinite (params.a));
     assert(isfinite (params.b));
     assert(isfinite (params.c));
     assert(isfinite (roots -> x1));
     assert(isfinite (roots -> x2));
-    assert(!isnan (params.a));
-    assert(!isnan (params.b));
-    assert(!isnan (params.c));
-    assert(!isnan (roots -> x1));
-    assert(!isnan (roots -> x2));
 
     int nRoots = solve_square (params, roots);
     switch (nRoots)
         {
-        case ZERO_ROOTS: printf(TEXT_COLOR(YELLOW_TEXT, "There are no roots\n"));
+        case ZERO_ROOTS: printf(SMALL_ERROR_COLOR("There are no roots\n"));
                 break;
 
-        case ONE_ROOT: printf(TEXT_COLOR(GREEN_TEXT, "x = %lf\n"), roots->x1);
+        case ONE_ROOT: printf(CORRECT_COLOR("x = %lf\n"), roots->x1);
                 break;
 
-        case TWO_ROOTS: printf(TEXT_COLOR(GREEN_TEXT, "x1 = %lf\tx2 = %lf\n"), roots -> x1, roots -> x2);
+        case TWO_ROOTS: printf(CORRECT_COLOR("x1 = %lf\tx2 = %lf\n"), roots -> x1, roots -> x2);
                 break;
 
-        case INF_ROOTS: printf(TEXT_COLOR(YELLOW_TEXT, "Number of roots: infinity\n"));
+        case INF_ROOTS: printf(SMALL_ERROR_COLOR("Number of roots: infinity\n"));
                 break;
 
         default:
